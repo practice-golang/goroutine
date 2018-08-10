987b5b35d759e0072235ccb2f30d0c036a728e28
Practice Golang <practice.golang@gmail.com>
2018-08-10 18:44:20 +0900

    주석 추가

--- src/goroutine/goroutine.go
+++ src/goroutine/goroutine.go
@@ -26,11 +26,14 @@ func main() {
 		defer waiter.Done()
 	}()
 
-	waiter.Add(1)
+	// waiter.Add(1)
 	go func(message string) {
 		say(message) // message에서 "안녕 세상!" 받아서 say로 전달 & 출력
 		defer waiter.Done()
 	}("안녕 세상!")
 
+	// sync.WaitGroup를 사용해서 고루틴 실행이 끝날 때까지 기다리게 한다.
+	// 희한한 건 sync.WaitGroup 대신 아래의 그냥 say()를 실행해도 고루틴이 실행 된다.
 	waiter.Wait()
+	// say("End")
 }
